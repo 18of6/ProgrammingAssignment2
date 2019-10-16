4905f90a8e54a6c209268585c2692609d8d1b7ab
18of6 <56653263+18of6@users.noreply.github.com>
2019-10-17 00:29:53 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  var1 <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    var1 <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inverse) var1 <<- inverse
+  getinverse <- function() var1
+  list(set = set,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+##This function computes the inverse of the special "matrix" returned by
+##makeCacheMatrix above. If the inverse has already been calculated 
+##(and the matrix has not changed), then cacheSolve should retrieve 
+##the inverse from the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  var1 <- x$getinverse()
+  if(!is.null(var1)) {
+    message("getting cached data.")
+    return(var1)
+  }
+  data <- x$get()
+  var1 <- solve(data)
+  x$setinverse(var1)
+  var1
 }
+
+## let's try it
+
+## 1st, we need a matrix
+## MAT <- matrix(c(1,3,5,7),2,2)
+
+## iMAT <- makeCacheMatrix(MAT)
+## cacheSolve(iMAT)
+##       [,1]   [,2]
+##[1,] -0.875  0.625
+##[2,]  0.375 -0.125
+
+## cacheSolve(iMAT)
+## getting cached data.
+##        [,1]   [,2]
+## [1,] -0.875  0.625
+## [2,]  0.375 -0.125
